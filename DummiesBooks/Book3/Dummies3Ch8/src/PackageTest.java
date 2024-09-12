4a8a91e9240dfe3a182dded2fd3824a3a059fb25
Eben Janse van Rensburg <ebenj.van.rensburg@gmail.com>
2024-09-12 16:08:04 +0200

    Dummies Book 3 Chapter 8

---
+++ DummiesBooks/Book3/Dummies3Ch8/src/PackageTest.java
@@ -0,0 +1,9 @@
+import com.gmail.markdrinkwater.util.Console;
+
+public class PackageTest {
+    public static void main(String[] args) {
+        while (Console.askYorN("Keep going?")) {
+            System.out.println("D'oh!");
+        }
+    }
+}
